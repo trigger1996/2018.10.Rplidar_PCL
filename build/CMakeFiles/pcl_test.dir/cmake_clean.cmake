45462ec49114087189f9a410cf97495e15b2dab1
trigger1996 <529388771@qq.com>
2018-10-09 09:42:35 +0800

    lidar & pcl integration finished

---
+++ build/CMakeFiles/pcl_test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl_test.dir/sdk/src/rplidar_driver.cpp.o"
+  "CMakeFiles/pcl_test.dir/sdk/src/arch/linux/net_serial.cpp.o"
+  "CMakeFiles/pcl_test.dir/sdk/src/arch/linux/net_socket.cpp.o"
+  "CMakeFiles/pcl_test.dir/sdk/src/arch/linux/timer.cpp.o"
+  "CMakeFiles/pcl_test.dir/sdk/src/hal/thread.cpp.o"
+  "CMakeFiles/pcl_test.dir/main.cpp.o"
+  "pcl_test.pdb"
+  "pcl_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
